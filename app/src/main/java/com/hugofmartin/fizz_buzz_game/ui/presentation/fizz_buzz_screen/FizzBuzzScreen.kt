f9e2cfcb24b8577bbba30ceb6072e767cebc1ac8
HugoFMartin <hugofabien.martin@gmail.com>
2022-08-30 20:29:49 +0200

    use extern composable fun for list item

--- app/src/main/java/com/hugofmartin/fizz_buzz_game/ui/presentation/fizz_buzz_screen/FizzBuzzScreen.kt
+++ app/src/main/java/com/hugofmartin/fizz_buzz_game/ui/presentation/fizz_buzz_screen/FizzBuzzScreen.kt
@@ -1,10 +1,15 @@
 package com.hugofmartin.fizz_buzz_game.ui.presentation.fizz_buzz_screen
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun FizzBuzzScreen(
@@ -12,11 +17,45 @@ fun FizzBuzzScreen(
 ) {
     val state = viewModel.state
 
-    Box(modifier = Modifier) {
-        state.value.list.forEach {
-            Column() {
-                Text(text = it)
+    Column(modifier = Modifier.fillMaxSize()) {
+        Text(
+            modifier= Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            text = "Résultat",
+            color = MaterialTheme.colors.primary,
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.h2
+        )
+        LazyColumn() {
+            items(
+                state.value.list
+            ) {
+                ListItem(
+                    Modifier.fillMaxWidth(),
+                    it
+                )
             }
         }
+
+    }
+}
+
+@Composable
+fun ListItem(
+    modifier: Modifier,
+    item: String
+) {
+    Row(
+        modifier = modifier
+            .padding(4.dp)
+    ) {
+        Text(
+            modifier =modifier,
+            text = item,
+            color = MaterialTheme.colors.primary,
+            textAlign = TextAlign.Center,
+            fontSize = 15.sp
+        )
     }
 }
